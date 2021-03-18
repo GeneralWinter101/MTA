177153b40d08c0132a11a39883e0f508846fab43
Karolina <karolina.romanska@hotmail.com>
2021-03-18 19:04:22 +0100

    MTA zadanie 1

---
+++ main.js
@@ -0,0 +1,42 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBicRKx6o6OQlI2P7LLjFhLXi8HzR4fSI4",
+    authDomain: "mtaassignment1.firebaseapp.com",
+    databaseURL: "https://mtaassignment1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mtaassignment1",
+    storageBucket: "mtaassignment1.appspot.com",
+    messagingSenderId: "715820354460",
+    appId: "1:715820354460:web:c3be3f53b9e9c5f0689659"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var messagesRef = firebase.database().ref();
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+	e.preventDefault();
+	const name = getInputVal("name");
+	const email = getInputVal("email");
+	const message = getInputVal("message");
+
+	saveMessage(name, email, message);
+	document.querySelector('.alert').style.display = 'block';
+	setTimeout(function() {
+		document.querySelector('.alert').style.display = 'none';
+	},3000);
+	document.getElementById('contactForm').reset();
+}
+
+function getInputVal(id) {
+	return document.getElementById(id).value;
+}
+
+function saveMessage(name, email, message) {
+	let newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		name: name,
+		email: email,
+		message: message
+	});
+}
